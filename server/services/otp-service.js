3454e37109ba2e9f335159d2a00c2b2d7b912685
Anubhav Srivastava <srivastavaanubhav764@gmail.com>
2022-04-05 22:36:41 +0530

    Setting up config variables and OTP twilio including

--- server/services/otp-service.js
+++ server/services/otp-service.js
@@ -1,4 +1,10 @@
 const crypto = require('crypto');
+
+
+
+const smsSid = process.env.SMS_SID;
+const smsAuthToken = process.env.SMS_AUTH_TOKEN
+const twilio = require('twilio')(smsSid)
 class OtpService{
 
 	async generateOtp(){
